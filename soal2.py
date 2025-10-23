fc4f025e074a23ff903cb9845ecb951a6ea894ea
nurullahahmad29-rgb <nurullahahmad29@gmail.com>
2025-10-23 15:28:25 +0700

    Create soal2.py with vehicle attributes list
    Add a list of vehicle attributes and modify it.

---
+++ soal2.py
@@ -0,0 +1,7 @@
+kendaraan = ["namaKendaraan", "jenisKendaraan", "ccKendaraan", "warnaKendaraan", "rodaKendaraan"]
+
+kendaraan.extend(["hargaNomor", "tipeKendaraan"])
+
+kendaraan.insert(2, "merkKendaraan")
+
+print(kendaraan)
